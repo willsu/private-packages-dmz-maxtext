8296ac50581e7ce1c10abb84b6402fde0b1ed8e2
Will Sulzer <willsulzer@google.com>
2024-08-27 18:58:52 +0000

    removed tmp scripts

--- tmp.sh
+++
@@ -1,5 +0,0 @@
-gcloud artifacts packages list \
-  --repository=pip-outer-loop \
-  --project=maxtext-qa-outer-8 \
-  --location=us-central1
-gcloud artifacts versions list --package=projects/maxtext-qa-outer-8/locations/us-central1/repositories/pip-outer-loop/packages/zipp --location us-central1 --repository pip-outer-loop/ --format "value(name)"
